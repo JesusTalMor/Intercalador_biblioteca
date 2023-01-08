e26e42b8813a23accbd6dced22be2d747b601d30
Jesus Talamantes <a01706335@itesm.mx>
2023-01-08 13:25:50 -0600

    Cambios a pop_ups

--- pop_ups.py
+++ pop_ups.py
@@ -3,61 +3,61 @@ from tkinter import messagebox
 
 # # ? POP_up de advertencia (Warning)
 ##########################################################
-def pop_warning_excel_file():
+def warning_excel_file():
   msg = f'!Fallo!\nSeleccione un archivo de Excel'
   messagebox.showwarning("No Excel File", msg)
 
-def pop_warning_excel_file_data_error():
+def warning_excel_file_data_error():
   msg = f'!Advertencia!\n Algunas Etiquetas no logran cargarse'
   messagebox.showwarning("File Data Warning", msg)
 
-def pop_warning_folder():
+def warning_folder():
   msg = f'!Fallo!\nSeleccione una carpeta de Salida'
   messagebox.showwarning("No Folder", msg)
 
-def pop_warning_name():
+def warning_name():
   msg = f'!Advertencia!\n Dele nombre al archivo de Salida'
   messagebox.showwarning("No Name", msg)
 
-def pop_warning_option():
+def warning_option():
   msg = f'!Advertencia!\n Seleccione alguna opción'
   messagebox.showwarning("No Option", msg)
 
-def pop_warning_clas():
+def warning_clas():
   msg = f'!Advertencia!\n Algunas clasificaciones tienen error.\n Modifique para continuar'
   messagebox.showwarning("Some Clas Errors", msg)
 ##########################################################
 
 # # ? POP_up de Error (Error)
 ##########################################################
-def pop_error_excel_file():
+def error_excel_file():
   msg = f'!Fallo!\n Ninguna Etiqueta se cargo'
   messagebox.showerror("Excel File Error", msg)
 ##########################################################
 
 # # ? POP_up de Informacion (Info)
 ##########################################################
-def pop_success_program():
+def success_program():
   msg = f'Felicidades\n El programa finalizó con exito'
   messagebox.showinfo("Finalize", msg)
 
-def pop_info_license():
+def info_license():
   msg = f'Programa Bajo Licencia del Tecnológico de Monterrey\n !No reproducir sin permiso!'
   messagebox.showinfo("Licencia", msg)
 
-def pop_info_about():
+def info_about():
   msg = f'Programa Generador de Etiquetas\n Por: Jesus Talamantes Morales 2022'
   messagebox.showinfo("About", msg)
 
 # # ? POP_up de OkCancel Ask (askokcancel)
 ##########################################################
-def pop_check_images():
+def check_images():
   msg = f'Imágenes Generadas\n Presione Aceptar para continuar con su PDF'
   answer = messagebox.askokcancel("Confirmar", msg)
   return answer
 
 if __name__ == "__main__":
-  print(pop_check_images())
+  print(check_images())
 
 
 
